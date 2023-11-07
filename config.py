d4cf805bb86896bf84620f964769735fef70661c
Ross Burton <ross.burton@arm.com>
2023-11-07 11:35:23 +0000

    Add builder for qemuarmv5
    Signed-off-by: Ross Burton <ross.burton@arm.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- config.py
+++ config.py
@@ -101,7 +101,7 @@ trigger_builders_wait_full = trigger_builders_wait_shared + [
     "qemumips-alt", "edgerouter-alt", "qemuppc-alt", "qemux86-world-alt",
     "oe-selftest-ubuntu", "oe-selftest-debian", "oe-selftest-fedora", "oe-selftest-centos",
     "qemux86-64-ptest", "qemux86-64-ltp", "qemuarm64-ptest", "qemuarm64-ltp",
-    "meta-intel", "meta-arm", "meta-aws", "meta-agl-core", "meta-virt"
+    "meta-intel", "meta-arm", "meta-aws", "meta-agl-core", "meta-virt", "qemuarmv5"
 ]
 
 trigger_builders_wait_quick_releases = {
@@ -223,6 +223,7 @@ builder_to_workers = {
 builder_tags = {
     "qemuarm":  ["qemu"],
     "qemuarm64":  ["qemu"],
+    "qemuarmv5":  ["qemu"],
     "qemumips": ["qemu"],
     "qemumips64": ["qemu"],
     "qemuppc": ["qemu"],
