0ab85784bed3c71dd44b714ef1452e226e7cf172
Ross Burton <ross.burton@arm.com>
2024-01-30 14:03:45 +0000

    config.py: upgrade qt6 to 6.6.0
    meta-webosose upgraded to 6.6.0 since I first set up the autobuilder
    configuration, so update the branch name.
    
    Signed-off-by: Ross Burton <ross.burton@arm.com>
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ repos = {
                           "master"],
     "bitbake": ["ssh://git@push.openembedded.org/bitbake", "master"],
     "auto-upgrade-helper": ["ssh://git@push.yoctoproject.org/auto-upgrade-helper", "master"],
-    "meta-qt6": ["http://code.qt.io/yocto/meta-qt6.git", "6.5.2"],
+    "meta-qt6": ["http://code.qt.io/yocto/meta-qt6.git", "6.6.0"],
     "meta-qt4": ["ssh://git@push.yoctoproject.org/meta-qt4", "master"],
     "meta-qt3": ["ssh://git@push.yoctoproject.org/meta-qt3", "master"],
     "meta-mingw": ["ssh://git@push.yoctoproject.org/meta-mingw", "master"],
