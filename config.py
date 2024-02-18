488f6f2b810e2587a9fcc780d6477a5f6739ae07
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-02-18 17:37:42 +0000

    config: Also enable qemuriscv64-tc target
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- config.py
+++ config.py
@@ -111,16 +111,16 @@ trigger_builders_wait_full = trigger_builders_wait_shared + [
 ]
 
 trigger_builders_wait_quick_releases = {
-    "master" : trigger_builders_wait_quick + ["qemuriscv64"],
-    "scarthgap" : trigger_builders_wait_quick + ["qemuriscv64"],
+    "master" : trigger_builders_wait_quick + ["qemuriscv64", "qemuriscv64-tc"],
+    "scarthgap" : trigger_builders_wait_quick + ["qemuriscv64", "qemuriscv64-tc"],
     "zeus" : trigger_builders_wait_quick + ["mpc8315e-rdb"],
     "thud" : trigger_builders_wait_quick + ["mpc8315e-rdb"],
     "sumo" : trigger_builders_wait_quick + ["mpc8315e-rdb"]
 }
 
 trigger_builders_wait_full_releases = {
-    "master" : trigger_builders_wait_full + ["qemuriscv64"],
-    "scarthgap" : trigger_builders_wait_full + ["qemuriscv64"],
+    "master" : trigger_builders_wait_full + ["qemuriscv64", "qemuriscv64-tc"],
+    "scarthgap" : trigger_builders_wait_full + ["qemuriscv64", "qemuriscv64-tc"],
     "zeus" : trigger_builders_wait_full + ["mpc8315e-rdb-alt"],
     "thud" : trigger_builders_wait_full + ["mpc8315e-rdb-alt"],
     "sumo" : trigger_builders_wait_shared + ["qemumips-alt", "edgerouter-alt", "mpc8315e-rdb-alt", "qemuppc-alt", "qemux86-world-alt",
