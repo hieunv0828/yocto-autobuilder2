11a1e0a899b6a276d7f80154944454167db97f60
Richard Purdie <richard.purdie@linuxfoundation.org>
2024-07-12 23:42:59 +0100

    schedulers.py: Set indexing to every few hours to minimise console disruption for now
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- schedulers.py
+++ schedulers.py
@@ -542,7 +542,8 @@ schedulers.append(sched.Nightly(name='nightly-auh', branch='master', properties=
 
 # Run the indexing targets every 15 mins
 schedulers.append(sched.Nightly(name='sschduler-indexing', branch='master', properties=parent_default_props('indexing'),
-                  builderNames=['indexing'], minute=[0,15,30,45]))
+                  builderNames=['indexing'], hour=[0, 9, 12, 15, 18, 21]))
+#minute=[0,15,30,45]
 
 # If any of our sphinx docs branches change, trigger a build
 schedulers.append(sched.AnyBranchScheduler(name="yocto-docs-changed",
